2a565c3f040c8bf6ac14b2d7aaa73ff558710951
LorDev <Lorenzo.sallons93@gmail.com>
2023-12-14 19:07:49 +0100

    added story section on homepage

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,6 @@
 "use strict";
 
-let checkbox = document.querySelector("#options-hoverpause-checkbox");
-
-new Glide(".glide", {
+let imgArr = new Glide(".glide", {
     type: "carousel",
     perView: 1,
     autoplay: 8000,
@@ -10,3 +8,28 @@ new Glide(".glide", {
     animationTimingFunc: "cubic-bezier(.71,.35,.59,.83)",
     gap: 0,
 }).mount();
+
+// slide 2
+const slider = document.getElementById("slider");
+let imgArray = [
+    "https://placehold.co/600x400/png",
+    "https://placehold.co/800x400/png",
+    "https://placehold.co/500x400/png",
+];
+
+let curIndex = 0;
+const imgDuration = 3000;
+
+function slideShow() {
+    slider.className += "fadeOut";
+    setTimeout(function () {
+        slider.src = imgArray[curIndex];
+        slider.className = "";
+    }, 1000);
+    curIndex++;
+    if (curIndex == imgArray.length) {
+        curIndex = 0;
+    }
+    setTimeout(slideShow, imgDuration);
+}
+slideShow();
