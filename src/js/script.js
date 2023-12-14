2b29524bd3fb094264e9b73eafa88fe668b7b807
LorDev <Lorenzo.sallons93@gmail.com>
2023-12-14 21:10:54 +0100

    added carousel slider to story section on homepage

--- src/js/script.js
+++ src/js/script.js
@@ -9,27 +9,51 @@ let imgArr = new Glide(".glide", {
     gap: 0,
 }).mount();
 
-// slide 2
-const slider = document.getElementById("slider");
-let imgArray = [
-    "https://placehold.co/600x400/png",
-    "https://placehold.co/800x400/png",
-    "https://placehold.co/500x400/png",
-];
+// Slider
+window.addEventListener("load", () => {
+    autoSlide();
+});
 
-let curIndex = 0;
-const imgDuration = 3000;
-
-function slideShow() {
-    slider.className += "fadeOut";
-    setTimeout(function () {
-        slider.src = imgArray[curIndex];
-        slider.className = "";
-    }, 1000);
-    curIndex++;
-    if (curIndex == imgArray.length) {
-        curIndex = 0;
-    }
-    setTimeout(slideShow, imgDuration);
+function autoSlide() {
+    setInterval(() => {
+        slide(getItemActiveIndex() + 1);
+    }, 5000); // slide speed = 3s
 }
-slideShow();
+
+function slide(toIndex) {
+    const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+    const itemActive = document.querySelector(".carousel_item__active");
+
+    // check if toIndex exceeds the number of carousel items
+    if (toIndex >= itemsArray.length) {
+        toIndex = 0;
+    }
+
+    const newItemActive = itemsArray[toIndex];
+
+    // start transition
+    newItemActive.classList.add("carousel_item__pos_next");
+    setTimeout(() => {
+        newItemActive.classList.add("carousel_item__next");
+        itemActive.classList.add("carousel_item__next");
+    }, 20);
+
+    // remove all transition class and switch active class
+    newItemActive.addEventListener(
+        "transitionend",
+        () => {
+            itemActive.className = "carousel_item";
+            newItemActive.className = "carousel_item carousel_item__active";
+        },
+        {
+            once: true,
+        }
+    );
+}
+
+function getItemActiveIndex() {
+    const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+    const itemActive = document.querySelector(".carousel_item__active");
+    const itemActiveIndex = itemsArray.indexOf(itemActive);
+    return itemActiveIndex;
+}
