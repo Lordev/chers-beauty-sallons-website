d6fbd753cf5daf3f604ffc4c8246bd0d96274e79
LorDev <Lorenzo.sallons93@gmail.com>
2023-12-19 10:40:56 +0100

    added portfolio-slider to homepage

--- src/js/slider/slider.js
+++ src/js/slider/slider.js
@@ -1,7 +1,62 @@
-import { Navigation, Pagination, Autoplay, FreeMode } from "swiper/modules";
+import {
+    Navigation,
+    Pagination,
+    Autoplay,
+    EffectFade,
+    EffectCards,
+    FreeMode,
+} from "swiper/modules";
 
-export let swiperReview = {
-    modules: [Navigation, Pagination, Autoplay, FreeMode],
+("use strict");
+///////////////////////////////////////
+//* fadeSlider settings
+export let fadeSlider = {
+    modules: [Navigation, Pagination, Autoplay, EffectFade],
+    effect: "fade",
+    crossfade: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 4000,
+        // waitForTransition: true,
+        // pauseOnMouseEnter: true,
+    },
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        clickable: true,
+    },
+};
+
+//* cardSlider settings
+export let cardSlider = {
+    modules: [EffectCards, Autoplay, Pagination],
+    effect: "cards",
+    grabCursor: true,
+    cardsEffect: {
+        rotate: true,
+        perSlideRotate: 12,
+        perSlideOffset: 1,
+        slideShadows: true,
+    },
+    autoplay: {
+        delay: 4000,
+        waitForTransition: true,
+    },
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        clickable: true,
+    },
+};
+
+//* defaultSlider settings
+export let defaultSlider = {
+    modules: [Navigation, Pagination, Autoplay],
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -17,3 +72,23 @@ export let swiperReview = {
         clickable: true,
     },
 };
+
+//* rollSlider settings
+export let infiniteSlider = {
+    modules: [Autoplay, FreeMode],
+    loop: true,
+    freeMode: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    slidesPerView: "6",
+    loop: true,
+    autoplay: {
+        delay: 0.5,
+        disableOnInteraction: true,
+    },
+    // freeMode: true,
+    speed: 5000,
+    freeModeMomentum: false,
+};
+
+//////////////////////////////////
